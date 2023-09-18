6f214f40b340f69dbf3ab5f068be838ce06080f2
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2023-09-18 16:47:58 +0000

    Changed Format and U and Sigma work

--- SingleValueDecomp.py
+++ SingleValueDecomp.py
@@ -29,73 +29,86 @@ def Enter_matrix():
     return A
 
 
-def Solve_V_U(A):
+def Solve_U(eig_val, eig_vec):
     """
-    Takes matrix A and calulates the orthonormal matrix V and U
+    calulates the orthonormal matrix U
 
-    Args: A matrix titled (A)
+    Args: eig_val: eigen values, eig_vec: eigen vectors
 
-    Returns: a list containing a matrix V (nxn) and a matrix U (mxm) containing the orthonormal eigenvectors:
-    (A^T)*(A) and (A)*(A^T) respectively.
+    Returns: the U matrix
     """
-    X = []
-    AT = np.transpose(A, axes=None)
-    V = np.matmul(AT, A)  # A^T*A matrix
-    U = np.matmul(A, AT)  # A*A^T matrix
-    X = [V, U]
-    return X
+    # Sort eigen vectors and value and solve U
+    sorted_indices = np.argsort(eig_val)[::-1]
+    eigenvalues = eig_val[sorted_indices]
+    eigenvectors = eig_vec[:, sorted_indices]
+    U = eigenvectors / np.linalg.norm(eigenvectors, axis=0)
+    return U
 
 
-def Solve_sigma_condnum(V):
+def Solve_Sigma(W, eig_val):
     """
-    Takes matrix V or U (Both have same eigen values) and calulates the diagonal matix Sigma with roots of
-    positive eigen values. Also calculates Condtiion Number.
+    calulates the diagonal matrix sigma
 
-    Args: A matrix titled V (nxm)
+    Args: W: A*A^T, eig_val: eigen values
 
     Returns: A matrix Sig (diagnial matrix) containing r elements equal to the root of the positive eigen values
     """
-    Sigma = np.zeros(np.shape(V))
-    eig_val = np.linalg.eigvals(V)
-    # Check for positve eigen values
+    Sigma = np.zeros(np.shape(W))
+    # Check for positve eigen values and solve sigma
     for i in range(len(eig_val)):
         if eig_val[i] <= 0:
             return "Error Non-Positive or Zero Eigen Value" + str(eig_val[i])
         else:
             Sigma[i, i] = math.sqrt(eig_val[i])
+    return Sigma
+
+
+def Solve_V(A, eig_val, U):
+    """
+    calulates the orthonormal matrix V
+
+    Args: A: nxm matrix, eig_val: eigen values, U: orthonormal U matrix
+
+    Returns: the V matrix
+    """
+    U_1 = U[:, 0]
+    U_2 = U[:, 1]
+    return U_1
+
+
+def Solve_Condition(eig_val):
     # condition number is ||A||_2 * ||A^-1||_2 or |Lamda_max|/|Lamda_min|
     CondNum = np.max(eig_val) / np.min(eig_val)
-    return [Sigma, CondNum]
+    return CondNum
+
+
+def Solve_Ainv(Sigma):
+    Sigmainv = np.zeros(np.shape(Sigma))
+
+    # inverse of matrix Sigma is just the reciprical of the diagonal values in sigma
+    for i in range(len(Sigma)):
+        Sigmainv[i, i] = 1 / Sigma[i, i]
 
 
 def SVD():
     """
-    Returns all SVD matrices, condition number and the inverse given by A^-1= V*Sigma^{-1}*U^T
+    Returns a dictionary of all SVD matrices, condition number and the inverse given by A^-1= V*Sigma^{-1}*U^T
     """
     A = Enter_matrix()
-    X = Solve_V_U(A)
-    U = X[0]
-    V = X[1]
-    L = Solve_sigma_condnum(V)
-    Sigma = L[0]
-    Sigmainv = np.zeros(np.shape(Sigma))
-    CondNum = L[1]
-    # inverse of matrix Sigma is just the reciprical of the diagonal values in sigma
-    for i in range(len(Sigma)):
-        Sigmainv[i, i] = 1 / Sigma[i, i]
+    # A^T*A matrix
+    AT = np.transpose(A, axes=None)
+    W = np.matmul(AT, A)
+    # Eigen values and vectors
+    eig_val, eig_vec = np.linalg.eig(W)
+
+    # Solve matrices of SVD
+    U = Solve_U(eig_val, eig_vec)
+    Sigma = Solve_Sigma(W, eig_val)
+    V = Solve_V(W, eig_val, eig_vec)
+    print(V)
+    # Solve Ainv and Condition number of A
+    # Ainv = Solve_Ainv(Sigma)
+    CondNum = Solve_Condition(eig_val)
+
     soln = [U, Sigma, V, CondNum]
     return soln
-
-
-def main():
-    """Main entry point of the app"""
-    J = SVD()
-
-    print(J[1])
-    print(J[2])
-    print(J[3])
-
-
-if __name__ == "__main__":
-    """This is executed when run from the command line"""
-    main()
