31bc9d1462c4d88e9f25a1aff66884cb522c320b
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2023-09-23 19:41:02 +0000

    Fully works finally

--- SingleValueDecomp.py
+++ SingleValueDecomp.py
@@ -32,70 +32,16 @@ def Enter_matrix():
 def Check_eig(eig_val):
     """Checks to see if eigen value are positive"""
     for i in range(len(eig_val)):
-        if eig_val[i] <= 0:
-            return False
+        if eig_val[i] >= 0:
+            continue
         else:
-            return True
+            raise Exception("Error Non-Positive Eigen value")
+    return 1
 
 
-def Solve_V(eig_val, eig_vec):
-    """
-    calulates the orthonormal matrix V
-
-    Args: eig_val: eigen values, eig_vec: eigen vectors
-
-    Returns: the V matrix
-    """
-    # Sort eigen vectors and value and solve U
-    V = eig_vec / np.linalg.norm(eig_vec, axis=0)
-    return V
-
-
-def Solve_Sigma(eig_val):
-    """
-    calulates the diagonal matrix sigma
-
-    Args: aat: A*A^T, eig_val: eigen values
-
-    Returns: A matrix Sig (diagnial matrix) containing r elements equal to the root of the positive eigen values
-    """
-    # Check for positve eigen values and solve sigma
-    sorted_indices = np.argsort(eig_val)[::-1]
-    eig_val = eig_val[sorted_indices]
-    sing_vals = np.sqrt(eig_val)
-    Sigma = np.diag(sing_vals)
-
-    return Sigma
-
-
-def Solve_U(eig_val, eig_vec):
-    """
-    calulates the orthonormal matrix U
-
-    Args: Eigen values and eigen vectors
-
-    Returns: the U matrix
-    """
-    # Sort eigen vectors and value and solve U
-    sorted_indices = np.argsort(eig_val)[::-1]
-    eigenvalues = eig_val[sorted_indices]
-    eigenvectors = eig_vec[:, sorted_indices]
-    U = eigenvectors / np.linalg.norm(eigenvectors, axis=0)
-    return U
-
-
-def Solve_Ainv(U, Sigma, V):
-    Sigmainv = np.zeros(np.shape(Sigma))
-    # inverse of matrix Sigma is just the reciprical of the diagonal values in sigma
-    for i in range(len(Sigma)):
-        Sigmainv[i, i] = 1 / Sigma[i, i]
-    Ainv = np.dot(V, Sigmainv, np.transpose(U))
-    return Ainv
-
-
-def Solve_Condition(A, Ainv):
-    # condition number is ||A||_2 * ||A^-1||_2 or |Lamda_max|/|Lamda_min|
-    CondNum = np.linalg.norm(A, 2) * np.linalg.norm(Ainv, 2)
+def Solve_Condition(A, Ainverse):
+    # condition number is ||A||_2 * ||A^-1||_2
+    CondNum = np.linalg.norm(A, 2) * np.linalg.norm(Ainverse, 2)
     return CondNum
 
 
@@ -104,35 +50,36 @@ def SVD():
     Returns a dictionary of all SVD matrices, condition number and the inverse given by A^-1= V*Sigma^{-1}*U^T
     """
     A = Enter_matrix()
-    # A^T*A matrix
-    AT = np.transpose(A, axes=None)
-    aat = np.dot(A, AT)
-    ata = np.dot(AT, A)
-    # Eigen values and vectors
-    eig_val, eig_vec = np.linalg.eig(ata)
-    eig_val1, eig_vec1 = np.linalg.eig(aat)
+
+    # Compute A^T * A and A * A^T
+    ATA = np.dot(A.T, A)
+    AAT = np.dot(A, A.T)
+
+    # Compute the eigenvalues and eigenvectors of ATA and AAT
+    eigenvalues_ATA, eigenvectors_ATA = np.linalg.eig(ATA)
+    eigenvalues_AAT, eigenvectors_AAT = np.linalg.eig(AAT)
 
     # Check for all positive eigen values
-    check_pos = Check_eig(eig_val)
-    if check_pos != True:
-        return "Error Non-Positive or Zero Eigen Value"
+    Check_eig(eigenvalues_AAT)
 
-    # Solve matrices of SVD
-    # Multiply matrix U and V by negative one as negative and positive values were swapped due to sorted indices
-    U = Solve_U(eig_val1, eig_vec1)
-    Sigma = Solve_Sigma(eig_val)
-    V = Solve_V(eig_val, eig_vec)
+    # Compute the singular values and the left and right singular vectors
+    # Singular values are the square roots of the eigenvalues
+    singular_values = np.sqrt(eigenvalues_ATA)
+
+    # Sort the singular values and corresponding singular vectors in descending order
+    sorted_indices = np.argsort(singular_values)[::-1]
+    singular_values = singular_values[sorted_indices]
+    U = eigenvectors_AAT[:, sorted_indices]
+    Sigma = singular_values
+    Vt = eigenvectors_ATA[:, sorted_indices]
+
     # Solve Ainv and Condition number of A
-    CondNum = Solve_Condition(A, np.linalg.inv(A))
-    soln = [U, Sigma, V, CondNum]
+    Sigma_inv = np.diag(1 / singular_values)
+    Ainverse = Vt @ Sigma_inv @ U.T
+    CondNum = Solve_Condition(A, Ainverse)
+
+    # Construct the V matrix by taking the transpose of Vt
+    V = Vt.T
+
+    soln = [U, Sigma, V, Ainverse, CondNum]
     return soln
-
-
-def main():
-    """Main entry point of the app"""
-    J = SVD()
-    print(J)
-
-
-if __name__ == "__main__":
-    main()
